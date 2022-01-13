0803679ff40679aa3a01c7f7dcbb7f1ee0895ba2
JerryLivano <90676468+JerryLivano@users.noreply.github.com>
2022-01-14 00:59:07 +0700

    Create mainpage.js

---
+++ mainpage.js
@@ -0,0 +1,10 @@
+function show (){
+    $('.sidebar').css('left','0');
+    $('#cancel').css('left','195px');
+    $(section).css('margin-left','250px');
+}
+function hide (){
+    $('.sidebar').css('left','-250px');
+    $('#cancel').css('left','-195px');
+    $(section).css('margin-left','0');
+}
