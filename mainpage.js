135377f6c89016ca07df8f71492fab196dc55bfe
JerryLivano <90676468+JerryLivano@users.noreply.github.com>
2022-01-14 19:27:42 +0700

    Update mainpage.js

--- mainpage.js
+++ mainpage.js
@@ -8,3 +8,17 @@ function hide (){
     $('#cancel').css('left','-195px');
     $(section).css('margin-left','0');
 }
+function readmore(i) {
+    let dots = document.getElementById("dots"+i);
+    let moreText = document.getElementById("more"+i);
+    let btnText = document.getElementById("myBtn"+i);
+    if (dots.style.display == "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read More"; 
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read Less"; 
+        moreText.style.display = "inline";
+    }
+}
