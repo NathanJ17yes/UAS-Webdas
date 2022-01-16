25f1785a43f6adc7f81a460d58777e82f427aef2
NathanJ17yes <90676004+NathanJ17yes@users.noreply.github.com>
2022-01-16 21:12:10 +0700

    Create page3.js

---
+++ page3.js
@@ -0,0 +1,16 @@
+muncul()
+function muncul(){
+     let div = document.querySelector(".container")
+        $("#button").hide()
+            for(let i=1; i<=9; i++){
+                let gambar=document.createElement("img")
+                gambar.src = "assets/" + i + ".jpg"
+                gambar.setAttribute("class", "zoom")
+                div.appendChild(gambar)
+        }
+        mediumZoom(document.querySelectorAll('.zoom'),{
+            margin : 55,
+            background : 'whitesmoke',
+            scrollOffset : 0,
+})
+    } 
